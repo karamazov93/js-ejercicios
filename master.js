1fb1521f9fccce2edef6475c7e618b5b7ed75887
karamazov93 <112257052+karamazov93@users.noreply.github.com>
2024-12-03 12:42:42 -0300

    formularios
    validaciones en html 5, validaciones en js y manipulacion del dom

--- master.js
+++ master.js
@@ -1,167 +1,101 @@
+/*const btn= document.getElementById("btn")
+btn.addEventListener("click",(e)=>{
+  e.preventDefault()
+  verdad= input.checkValidity()
+  if (verdad) {
+      console.log("submit", "[" +input.value +"]",verdad);
+  }
+  else{
+    console.error("campo obligatorio")
+  }
+
+  console.log("click");
+  
+  
+})
+  
+
+
+const input= document.querySelector("input")
+const formulario= document.getElementById("form")
+
+/*formulario.addEventListener("submit",(e)=>{
+  e.preventDefault()
+  verdad= input.checkValidity()
+  if (verdad) {
+      console.log("submit", "[" +input.value +"]",verdad);
+  }
+  else{
+    console.error("campo obligatorio")
+  }
+
+  console.log("click");
+  
+  
+})*/
 
 
 
-const a= _=>{
-let random= Math.random() // 0 0.9999
-random*= 10 //0 y 9.9999
-random= parseInt(random)
+const form= document.querySelector("form")
+const input= document.querySelector("input")
 
-return ++random
+
+function setCustomValidity(mensaje) {
+  const div= document.querySelector("div")
+  div.innerText= mensaje
+  div.style.display= mensaje? "block": "none"
 }
 
-for(i=0; i<10; i++)console.log(a());
 
+function validarInput(valor) {
+  valor= valor.trim()
+  let mensaje=""
+ const lg= valor.length
+ if (lg==0) {
+  mensaje= "este campo es obligatorio"
+  console.error(mensaje);
+  setCustomValidity(mensaje)
 
-const b=()=> ({nombre:"nico", apellido:"jogelino"})    
+  return null //si el valor NO cumplió con las validaciones
+ }
+ else if(lg< 3){
+  mensaje= "este campo debe poseer minimo tres caracteres"
+  console.error(mensaje);
+  setCustomValidity(mensaje)
+return null
+ }
+ else if(lg> 10){
+  mensaje= "este campo debe poseer máximo diez caracteres"
+  console.error(mensaje);
+  setCustomValidity(mensaje)
+return null
+ }
+ else if(valor.includes(" ")){
+  mensaje= "error de validación, no permite espacios intermedios"
+  console.error(mensaje);
+  setCustomValidity(mensaje)
+return null
+ }
+setCustomValidity(mensaje)
 
-
-console.log(b());
-
-var fecha= ["17/11/2024"]
-
-const c=function(f){
-f[0]=f[0]+ " "+ "12:45"
-console.log(f);
-
+valor= valor.encodeURIcomponent(valor)
+  return valor
 }
 
-console.log(fecha);
-c(fecha)
-console.log(fecha);
-
-let titulo= document.querySelector(".titulo")
-titulo.addEventListener("mouseover",()=>{
-    titulo.classList.add("colores")
+addEventListener("input",()=>{
+  validarInput(input.value)
+  
 })
- const items= document.querySelectorAll('li')
 
+form.addEventListener("submit",(e)=>{
 
- const color= ["blue", "green", "000","#ff8000", "rgb(255,0,255)"]
-for(i=0; i<items.length; i++){
-    items[i].innerHTML=`elemento numero ${i+1}`
-   items[i].style.color=color[i]
-   items[i].style.backgroundColor=i%2==0 ? "#000": "#111"
+  e.preventDefault()
+let valor= validarInput(input.value)
+
+if (valor) {
+  console.log("valor ingresado", "["+valor +"]");
 }
 
-
-const suma = (a,b)=> a + b
-
-
-
-function operacion(a,b,cb) {
-  return  cb(a,b)
-    
-}
-let n1= 1, n2=5
-
-console.log(`la suma entre ${n2} y ${n1} es ${operacion(n1,n2, (x,t)=> x + t)}`)
-
-
-
-function uno(params) {
-    return console.log("soy la funcion uno");
-    
-}
-
-function dos() {
-    return console.log("soy la funcion 2");
-    
-}
-let btn= document.querySelector(".btn")
-
-
-btn.addEventListener("click", uno)
-btn.addEventListener("click", dos)
-
-setTimeout(()=>{
-    console.log("aca arranco",new Date().toLocaleString());
-btn.removeEventListener("click", dos)
-    console.log("aca termino", new Date().toLocaleString())
-    
-}, 3000)
-
-
-let link= document.getElementById("link")
-link.addEventListener("click", (e)=> {
-    e.preventDefault()
-   
-    console.log("Inicio", new Date().toLocaleString());
-    setTimeout(()=>{
-        window.location.href="http://www.google.com"
-        console.log("Fin", new Date().toLocaleString());
-        
-        
-    },2000)
+  
+  
 })
-
-
-//propagacion de eventos
-var one= document.getElementById("one")
-var doss= document.getElementById("dos")
-var tres= document.getElementById("tres")
-
-tres.addEventListener("click",()=>{
-    console.log("click en tres");
-    
-},true)
-doss.addEventListener("click",()=>{
-    console.log("click en dos");
-    
-},true)
-one.addEventListener("click",()=>{
-    console.log("click en uno");
-    
-},true)
-console.clear()
-
-var botonCreado= false
-
-var estatico= document.getElementById("estatico")
-
-estatico.addEventListener("click",()=>{
-    console.log("boton estático")
-    if (!botonCreado) {
-            var dinamico= document.createElement("button")
-    dinamico.innerHTML= "boton dinamico"
-    document.body.appendChild(dinamico)
-    dinamico.id="dinamico"
-
-    botonCreado= true
-    }
-    
-})
-
-
-document.addEventListener("click",(e)=>{
-    if (e.target.id == "dinamico") {
-          console.log("click en dinamico")
-    }
-    document.dispatchEvent(ev)
-})
-
-document.addEventListener("look",()=>{
-    console.log("look!")
-})
-var ev= new Event("look", {bubbles:true,cancelable:true})
-
-
-
-
-
-var btnlook= document.getElementById("btn-look")
-btnlook.addEventListener("click",()=>{
-    document.dispatchEvent(ev)
-})
-
-
-    
-var time= setInterval(()=>{
-   console.log( new Date().toLocaleString())
-    console.log("probando set interval")
-},3000)
-
-console.log(new Date().toLocaleString())
-setTimeout(()=>{
-console.log("programa terminado")
-clearInterval(time)
-},10000)
